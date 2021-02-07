0bd24d719a3cbe31f83004492dab638a1058a47e
Saltator <sebastian.krzus@gmail.com>
2021-02-07 09:35:19 +0100

    Fast check before exam

--- KolokwiumII/KolokwiumII/Program.cs
+++ KolokwiumII/KolokwiumII/Program.cs
@@ -6,7 +6,7 @@ namespace KolokwiumII
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("");
         }
     }
 }
